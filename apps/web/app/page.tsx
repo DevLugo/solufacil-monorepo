c9fecdbe233c0b0c04863431c71f345baa02ee2c
Lugo <bl1ndcod3@gmail.com>
2025-12-19 11:30:26 -0600

    feat: removed not neccesaru page.tsx

--- apps/web/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function Home() {
-  // Redirect to dashboard
-  // AuthGuard will redirect users to their role-appropriate home page if needed
-  redirect('/dashboard')
-}
